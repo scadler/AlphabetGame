61e7928f4fe7f19e59f884504e3a626b55aa54f2
scadler <scadler@s.sfusd.edu>
2020-04-14 20:23:28 +0000

    game finished, need to make it look better + more features

--- script.js
+++ script.js
@@ -13,10 +13,29 @@ $("#inputButton").click(function(){
     let regex = new RegExp(blackListLetter.letter);
     var test = regex.test(input)
     console.log(test)
+    if(test === true){
+        $("#input").css("background-color","#AAFFAA")
+         $("#input").css("border-color","#66AA66")
+         $("#attempts").text( Number($("#attempts").text()) + 1 )
+    }
+    else{
+         $("#input").css("background-color","#FFAAAA")
+         $("#input").css("border-color","#AA6666")
+        $("#attempts").text( Number($("#attempts").text()) + 1 )
+    }
 })
 $("#inputLetterButton").click(function(){
     var input = $("#inputLetter").val().toLowerCase()
     if(input === blackListLetter.letter){
-        console.log("works")
+        $("#inputLetter").css("background-color","#AAFFAA")
+         $("#inputLetter").css("border-color","#66AA66")
+        pickLetter()
+        $("#attempts").text( "0" )
+          $("#score").text( Number($("#score").text()) + 1 )
+    }
+    else{
+         $("#inputLetter").css("background-color","#FFAAAA")
+         $("#inputLetter").css("border-color","#AA6666")
+         $("#attempts").text( Number($("#attempts").text()) + 1 )
     }
 })
